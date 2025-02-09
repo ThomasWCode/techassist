739cafed30ab3cae30cad4c8919160fb4d42360b
ThomasWCode <thomasawhite321@gmail.com>
2025-02-09 17:11:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // Function to detect the user's browser
 function detectBrowser() {
     const userAgent = navigator.userAgent.toLowerCase();
-    if (userAgent.indexOf('chrome') > -1) {
+    
+    // Edge detection (new Edge based on Chromium)
+    if (userAgent.indexOf('edg') > -1) {
+        return 'edge';
+    } else if (userAgent.indexOf('chrome') > -1) {
         return 'chrome';
     } else if (userAgent.indexOf('firefox') > -1) {
         return 'firefox';
     } else if (userAgent.indexOf('safari') > -1) {
         return 'safari';
-    } else if (userAgent.indexOf('edge') > -1) {
-        return 'edge';
     } else {
         return 'other';
     }
@@ -17,15 +19,10 @@ function detectBrowser() {
 // Function to display browser-specific instructions in the modal
 function showBrowserInstructions(browser) {
     const instructionsElement = document.getElementById('browserInstructions');
-    
-    // Check for Edge specifically
-    if (navigator.userAgent.indexOf('Edge') !== -1) {
-        browser = 'edge'; // Force detection as Edge
-    }
 
     switch (browser) {
         case 'chrome':
-            instructionsElement.innerHTML = "<p>An down arrow should appear in the top right of Chrome. It will have a blue circle going round it.</p> <p>Once the download has finished (circle gone), click on the down arrow to open a small menu.</p> <p>Then hover over 'TechAssist_Setup.exe' and click on the arrow on the right point to the topleft with a box round it.</p>";
+            instructionsElement.innerHTML = "<p>A down arrow should appear in the top right of Chrome. It will have a blue circle going round it.</p> <p>Once the download has finished (circle gone), click on the down arrow to open a small menu.</p> <p>Then hover over 'TechAssist_Setup.exe' and click on the arrow on the right pointing to the top-left with a box around it.</p>";
             break;
         case 'firefox':
             instructionsElement.innerHTML = '<p>In Mozilla Firefox: After downloading, click the down arrow in the top right to open the downloaded file or go to the Downloads section in the menu.</p>';
@@ -34,7 +31,7 @@ function showBrowserInstructions(browser) {
             instructionsElement.innerHTML = '<p>In Microsoft Edge: After downloading, click the down arrow in the top right or go to your Downloads folder.</p>';
             break;
         default:
-            instructionsElement.innerHTML = '<p>After downloading, open file explorer, go to your downloads and double-left-click on TechAssist_Setup.exe</p>';
+            instructionsElement.innerHTML = '<p>After downloading, open file explorer, go to your downloads, and double-left-click on TechAssist_Setup.exe</p>';
             break;
     }
 }
